e544b51474c7f63c102075013460c8a7ab5f4d4e
eesha <79874478+beepboopblorp@users.noreply.github.com>
2024-09-15 20:04:09 -0400

    Add files via upload

--- matching_game.js
+++ matching_game.js
@@ -1,7 +1,8 @@
-let cardtype;
+let cardtype, randnum, cardid;
+let randnums = [], cardnums = [], artnames = [], emptycards = [];
 
 class info {
-    constructor(artnum, name, date, material, form, content, art_function, context) {
+    constructor(artnum = "", name = "", date = "", material = "stone", form = "pretty", content = "cat", art_function = "death", context = "trade") {
         this.name = name;
         this.date = date;
         this.material = material;
@@ -11,92 +12,140 @@ class info {
         this.context = context;
         this.artnum = artnum;
     }
-    addtocard(type) {
-        let randcard = Math.floor(Math.random() * cards.length);
-        cards[randcard].innerText = this.name + " " + type;
-        cards.splice(randcard,1);
-        art.splice(this.artnum,1);
-    }
 }
-
-class card {
-    constructor(cardid, cnum) {
-        this.cardid = cardid;
-        this.cnum = cnum;
-    }
-    addart(type) {
-        let randart = Math.floor(Math.random() * art.length);
-            switch (cardtype) {
-                case "date":
-                    this.cardid.innerText = art[randart].date;
-                    cards.splice(this.cnum,1);
-                    break;
-                case "material":
-                    this.cardid.innerText = art[randart].material;
-                    cards.splice(this.cnum,1);
-                    break;
-                case "form":
-                    this.cardid.innerText = art[randart].form;
-                    cards.splice(this.cnum,1);
-                    break;
-                case "content":
-                    this.cardid.innerText = art[randart].content;
-                    cards.splice(this.cnum,1);
-                    break;   
-                case "function":
-                    this.cardid.innerText = art[randart].function;
-                    cards.splice(this.cnum,1);
-                    break;
-                case "context":
-                    this.cardid.innerText = art[randart].context;
-                    cards.splice(this.cnum,1);
-                    break;
-            }
-        art[randart].addtocard(cardtype);
-    }
-}
-
-let cards = [new card(document.getElementById("card1"),0), new card(document.getElementById("card2"),1), new card(document.getElementById("card3"),2), new card(document.getElementById("card4"),3), new card(document.getElementById("card5"),4), new card(document.getElementById("card6"),5), new card(document.getElementById("card7"),6), new card(document.getElementById("card8"),7), new card(document.getElementById("card9"),8), new card(document.getElementById("card10"),9), new card(document.getElementById("card11"),10), new card(document.getElementById("card12"),11), new card(document.getElementById("card13"),12), new card(document.getElementById("card14"),13), new card(document.getElementById("card15"),14), new card(document.getElementById("card16"),15), new card(document.getElementById("card17"),16), new card(document.getElementById("card18"),17)];
 
 let art = [new info(0,"red",1900), new info(1,"orange",1910), new info(2,"yellow",1920), new info(3,"green",1930), new info(4,"blue",1940), new info(5,"indigo",1950), new info(6,"violet",1960), new info(7,"black",1970), new info(8,"brown",1980), new info(9,"white",1990), new info(10,"pink",2000)];
 
-let forms = [art[0].form, art[1].form, art[2].form, art[3].form, art[4].form, art[5].form, art[6].form, art[7].form, art[8].form, art[9].form, art[10].form];
-
-let contents = [art[0].content, art[1].content, art[2].content, art[3].content, art[4].content, art[5].content, art[6].content, art[7].content, art[8].content, art[9].content, art[10].content];
-
-let functions = [art[0].function, art[1].function, art[2].function, art[3].function, art[4].function, art[5].function, art[6].function, art[7].function, art[8].function, art[9].function, art[10].function];
-
-let contexts = [art[0].context, art[1].context, art[2].context, art[3].context, art[4].context, art[5].context, art[6].context, art[7].context, art[8].context, art[9].context, art[10].context];
-
-let dates = [art[0].date, art[1].date, art[2].date, art[3].date, art[4].date, art[5].date, art[6].date, art[7].date, art[8].date, art[9].date, art[10].date];
-
-let materials = [art[0].material, art[1].material, art[2].material, art[3].material, art[4].material, art[5].material, art[6].material, art[7].material, art[8].material, art[9].material, art[10].material];
-
-if (document.getElementById("submit_type")) {
-    document.getElementById("submit_type").addEventListener("click", setcardtype);
-}
+if (document.getElementById("submit_type")) {document.getElementById("submit_type").addEventListener("click", setcardtype);}
 
 function setcardtype() {
     let properties = document.getElementsByName("select_type");
-    for (i=0; i<properties.length; i++) {
+    for (let i=0; i<properties.length; i++) {
         if (properties[i].checked) {
             cardtype = properties[i].value;
-        }
+        } // add an html red error  message
     }
 
     if (cardtype) {
         console.log(cardtype);
         
         let choosetype = document.getElementsByName("select_type");
-        for (i=0; i<choosetype.length; i++) {choosetype[i].style.display = "none";}
+        for (let i=0; i<choosetype.length; i++) {choosetype[i].style.display = "none";}
 
         let cardslist = document.getElementsByClassName("matchcard");
-        for (i=0; i<cardslist.length; i++) {cardslist[i].style.display = "inline";}
-    
-        while(cards.length > 0) {
-            cards[0].addart(cardtype);
+        for (let i=0; i<cardslist.length; i++) {cardslist[i].style.display = "inline";}
+
+        randassign();
+        randcardassign();
+        assigntocard();
+    }
+
+    for (let i=0; i < 18; i++) {
+        let cardslist = document.getElementsByClassName("matchcard");
+        console.log(cardslist[i].innerText);
+    }
+}
+
+function randassign() {
+    while (randnums.length < 10) {
+        randnum = Math.floor(Math.random() * art.length);
+        if (!randnums.includes(randnum)) {
+            randnums.push(randnum);
+        }
+    }
+
+    randnums.splice((randnums.length - 1),1);
+}
+
+function randcardassign() {
+    while (cardnums.length < 10) {
+        randnum = Math.floor(Math.random() * 18);
+        if (!cardnums.includes(randnum)) {
+            cardnums.push(randnum);
+        }
+    }
+
+    cardnums.splice((cardnums.length - 1),1);
+}
+
+function assigntocard() {
+    for (let i=0; i < 9; i++) {
+        if (cardnums[i]) {
+            cardid = "card" + (cardnums[i] + 1);
+        } else if (cardnums[i] === 0) {
+            cardid = "card1"
         }
 
-        for (i=0; i<cardslist.length; i++) {console.log(cardslist[i].innerText);}
-    } //add error message later
+        if (cardid) {
+            switch (cardtype) {
+                case "dates":
+                    document.getElementById(cardid).innerText = art[randnums[i]].date;
+                    break;
+
+                case "materials":
+                    document.getElementById(cardid).innerText = art[randnums[i]].material;
+                    break;   
+
+                case "forms":
+                    document.getElementById(cardid).innerText = art[randnums[i]].form;
+                    break;
+
+                case "contents":
+                    document.getElementById(cardid).innerText = art[randnums[i]].content;
+                    break;
+
+                case "functions":
+                    document.getElementById(cardid).innerText = art[randnums[i]].function;
+                    break;
+                
+                case "contexts":
+                    document.getElementById(cardid).innerText = art[randnums[i]].context;
+                    break;
+            }
+
+            artnames.push(art[randnums[i]].name);
+        }
+    }
+
+    for (let i=0; i<18; i++) {
+        cardid = "card" + (i + 1);
+        if (!document.getElementById(cardid).innerText) {
+            emptycards.push(cardid);
+        }
+    }
+
+    for (let i=0; i < 9; i++) {
+        switch (cardtype) {
+            case "dates":
+                document.getElementById(emptycards[i]).innerText = artnames[i] + " date";
+                break;
+
+            case "materials":
+                document.getElementById(emptycards[i]).innerText = artnames[i] + " materials";
+                break;   
+
+            case "forms":
+                document.getElementById(emptycards[i]).innerText = artnames[i] + " forms";
+                break;
+
+            case "contents":
+                document.getElementById(emptycards[i]).innerText = artnames[i] + " contents";
+                break;
+
+            case "functions":
+                document.getElementById(emptycards[i]).innerText = artnames[i] + " functions";
+                break;
+            
+            case "contexts":
+                document.getElementById(emptycards[i]).innerText = artnames[i] + " contexts";
+                break;
+        }
+    }
+}
+
+if (document.getElementsByClassName("matchcard")) {document.getElementsByClassName("matchcard").addEventListener("click", clickyfuntime)}
+let ccc = 0; //ccc = clicked card counter
+
+function clickyfuntime() {
+    
 }
